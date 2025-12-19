30b6a7a64e67e1aa675f82003d8ed831024e8e86
aoluoch <aoluoch271@gmail.com>
2025-12-19 21:04:17 +0300

    update pachages

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { sentryVitePlugin } from '@sentry/vite-plugin'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    sourcemap: true,
-  },
   plugins: [
     react(),
     tailwindcss(),
-    sentryVitePlugin({
-      org: process.env.SENTRY_ORG,
-      project: process.env.SENTRY_PROJECT,
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    }),
   ],
   resolve: {
     alias: {
